4031dc61f90b3a005bdae22f4cb9a9823c03d0d2
maya-dixon <77749708+maya-dixon@users.noreply.github.com>
2021-02-12 20:48:46 -0500

    Add files via upload

--- COVIDgenome.R
+++ COVIDgenome.R
@@ -3,4 +3,4 @@ nchar(COVIDseq, type = "chars")
 #there are 31,602 characters in the sequence and we are looking for S protein which starts at character 21,563 and ends at character 25,384
 Sprotein <- substr(COVIDseq, 21563, 25384)
 #Would you say this gene is highly conserved or evolving rapidly? Why?  Explain this in a comment at the end of your second script.
-#I would say that based of the data from my BLAST search
+#I would say that based of the data from my BLAST search is highly conserved because percent identity section is quite close to 100% and there are many matches which leads me to believe that it is highly conserved.
