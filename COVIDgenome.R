d041c5cec5e65a4c7290cdf0baeadea152a4860d
maya-dixon <77749708+maya-dixon@users.noreply.github.com>
2021-02-12 20:45:16 -0500

    Add files via upload

---
+++ COVIDgenome.R
@@ -0,0 +1,6 @@
+COVIDseq <-read.csv("./COVID sequence.csv")
+nchar(COVIDseq, type = "chars")
+#there are 31,602 characters in the sequence and we are looking for S protein which starts at character 21,563 and ends at character 25,384
+Sprotein <- substr(COVIDseq, 21563, 25384)
+#Would you say this gene is highly conserved or evolving rapidly? Why?  Explain this in a comment at the end of your second script.
+#I would say that based of the data from my BLAST search
--- Download.R
+++ Download.R
@@ -1,7 +1,10 @@
 library(dplyr)
 ncbi_ids <- c("HQ433692.1","HQ433694.1","HQ433691.1")
+# This is assigning the ID names of each of the 3 sequences to a variable called ncbi_ids
 library(rentrez)  # you may need install.packages first
+# This is loadign the rentrez package into the r script
 Bburg <-entrez_fetch(db = "nuccore", id = ncbi_ids, rettype = "fasta")
+# entrez_fetch allows you to retried full data from the NCBI database, db is the database name, id are the unique id's within the object and rettype allows your to specify how you would like the sequence formatted.
 
 print(Bburg)
 
@@ -11,12 +14,12 @@ str(Bburg)
 
 #Use the strsplit() function on the Bburg object to create a new object
 
-Sequences <- strsplit(Bburg, split = "\n\n")
+Sequences <- strsplit(Bburg, split = "\n\n") #splitting the sequences
 print(Sequences)
 
 Sequences<-unlist(Sequences)
 
-header<-gsub("(^>.*sequence)\\n[ATCG].*","\\1",Sequences)
+header<-gsub("(^>.*sequence)\\n[ATCG].*","\\1",Sequences) 
 seq<-gsub("^>.*sequence\\n([ATCG].*)","\\1",Sequences)
 Sequences<-data.frame(Name=header,Sequence=seq)
 print(Sequences)
